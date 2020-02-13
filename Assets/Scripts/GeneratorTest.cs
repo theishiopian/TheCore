440ac06156b2d15f80fcd63b8cdc06d4b470d0de
theishiopian <theishiopian@gmail.com>
2020-02-12 16:14:44 -0800

    Procgen test 1

--- Assets/Scripts/GeneratorTest.cs
+++ Assets/Scripts/GeneratorTest.cs
@@ -1,18 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class GeneratorTest : MonoBehaviour
 {
+    public int depth;
+    public TileBase fillTile;
+
+    private Tilemap map;
+    private Grid grid;
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        map = this.GetComponent<Tilemap>();
+        grid = map.layoutGrid;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        //map.SetTile(new Vector3Int(0,0,0), fillTile);
+
+        for (int y = 0; y != depth; y++)
+        {
+            //map.SetTile(new Vector3Int(0, -y, 0), fillTile);
+            for(int i = -7; i != 7; i++)
+            {
+                map.SetTile(new Vector3Int(i, -y, 0), fillTile);
+            }
+        }
     }
 }
