51f94954ab345d4afca361050ea2a09577d0a59d
theishiopian <theishiopian@gmail.com>
2020-02-20 09:16:39 -0800

    Overhauled player controller, part 1

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -19,23 +19,35 @@ public class PlayerController : MonoBehaviour
     }
 
     float x;
+    float y;
     float cameraVelocity;
-    // Update is called once per frame
-    void FixedUpdate()
+    bool jump = false;
+
+    private void Update()
     {
         x = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
-
-        body.position += new Vector2(x, 0);
-
+        y = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
         if(Input.GetKeyDown(KeyCode.Space))
         {
-            if(Physics2D.Raycast(this.transform.position, Vector2.down, castDistance))
+            RaycastHit2D hit = Physics2D.CircleCast(this.transform.position, 0.5f, Vector2.down, 0.6f);
+            if(hit.collider.CompareTag("Blocks"))
             {
-                body.AddForce(new Vector2(0, 5), ForceMode2D.Impulse);
+                jump = true;
             }
         }
+    }
 
-        if(transform.position.y < 0)
+    void FixedUpdate()
+    {
+        body.position += new Vector2(x, 0);
+
+        if (jump)
+        {
+            jump = false;
+            body.AddForce(new Vector2(0, 5), ForceMode2D.Impulse);
+        }
+
+        if (transform.position.y < 0)
         {
             var y = Mathf.SmoothDamp(camera.transform.position.y, this.transform.position.y, ref cameraVelocity, Time.deltaTime, maxCameraSpeed);
             camera.transform.position = new Vector3(0, y, -10);
