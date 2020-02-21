08054ef67cb932ae69bc49c29074bc36de901b6d
theishiopian <theishiopian@gmail.com>
2020-02-20 18:21:06 -0800

    Implemented item
    gently bounces. magic!

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    public AnimationCurve bounce;
+    public float offset = 0.125f;
+
+    private Vector3 topPos;
+    private Vector3 bottomPos;
+    private Transform sprite;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        topPos = new Vector3(0, offset, 0);
+        bottomPos = new Vector3(0, offset, 0);
+        sprite = this.transform.Find("Sprite");
+    }
+
+    private float t = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        t += Time.deltaTime;
+        t = Mathf.Repeat(t, 1);
+        sprite.localPosition = new Vector3(0, bounce.Evaluate(t), 0) * 0.125f;
+    }
+}
