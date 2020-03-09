1beda8732aaffd57c96aaab9e160fb4510d66ad8
mhkawabata <marcy.kawabata@gmail.com>
2020-03-08 23:42:29 -0700

    starting HUD stuff

---
+++ Assets/Scripts/PlayerHUD.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHUD : MonoBehaviour
+{
+    public GameObject thePlayer;
+    public Slider expBar;
+    float maxExp = 1;
+    float currentExp = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        expBar.value = CalculateExp();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentExp = GlobalVars.xp;
+    }
+
+    float CalculateExp()
+    {
+        return currentExp / maxExp;
+    }
+
+}
