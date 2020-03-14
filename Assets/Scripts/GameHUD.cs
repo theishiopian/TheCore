3d956766a9cbd74d50f6f6850fddb1e05ddedf71
theishiopian <theishiopian@gmail.com>
2020-03-13 17:27:18 -0700

    Fixed Depth gauge

--- Assets/Scripts/GameHUD.cs
+++ Assets/Scripts/GameHUD.cs
@@ -12,21 +12,25 @@ public class GameHUD : MonoBehaviour
     //TODO: hook up depth and depthMax to corresponding variables in our game,
     //      change arrow rotation to change with player's depth
     private float depthMax;
-    private float depth;
 
-    private void Awake()
+    private GameObject player;
+    private MapGenerator gen;
+
+    private void Start()
     {
-        arrowTransform = transform.Find("arrow");
-
-        depth = 0f;
-        depthMax = 200f;
+        player = GlobalVars.GetObject("player");
+        gen = GlobalVars.GetObject("grid").GetComponent<MapGenerator>();
+        arrowTransform = GlobalVars.GetObject("needle").transform;
+        depthMax = gen.depth;
     }
 
     private void Update()
     {
-        //for testing functionality
-        depth += 30f * Time.deltaTime;
-        if (depth > depthMax) depth = depthMax;
+        ////for testing functionality
+        //depth += 30f * Time.deltaTime;
+
+
+        //if (depth > depthMax) depth = depthMax;
 
         //arrow angle corresponds to depth change
         arrowTransform.eulerAngles = new Vector3(0, 0, GetDepthRotation());
@@ -34,9 +38,14 @@ public class GameHUD : MonoBehaviour
 
     private float GetDepthRotation()
     {
-        float totalAngleSize = ZERO_DEPTH_ANGLE - MAX_DEPTH_ANGLE;
-        float depthNormalized = depth / depthMax;
-        return ZERO_DEPTH_ANGLE - depthNormalized * totalAngleSize;
+        //float totalAngleSize = ZERO_DEPTH_ANGLE - MAX_DEPTH_ANGLE;
+        //float depthNormalized = Mathf.Abs(depth) / depthMax;
+
+        float t = 0;
+
+        t = Mathf.Abs(player.transform.position.y) / depthMax;
+
+        return -Mathf.Lerp(MAX_DEPTH_ANGLE, ZERO_DEPTH_ANGLE, t);
     }
 
 }
